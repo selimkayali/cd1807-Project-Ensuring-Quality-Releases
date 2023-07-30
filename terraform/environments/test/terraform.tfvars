799dbd5cb03b5371726bd2d7f84f2a3bec932d0f
selimkayali <selimkayali@gmail.com>
2023-07-30 15:09:39 +0300

    init changes

--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,15 +1,15 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "e2bf10f5-b611-44c7-9617-4a26610fc460"
+client_id       = "3cc71462-d680-4923-8a8b-e63f26e9aa3d"
+client_secret   = "jMu8Q~ihOt8XMrJeZXl0PyzNmx2~__vOWUekkciX"
+tenant_id       = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 
 # Resource Group/Location
-location = ""
-resource_group_name = ""
-application_type = ""
+location         = "East US"
+resource_group   = "Azuredevops"
+application_type = "myApplication"
 
 # Network
 virtual_network_name = ""
-address_space = ["10.5.0.0/16"]
-address_prefix_test = "10.5.1.0/24"
+address_space        = ["10.5.0.0/16"]
+address_prefix_test  = "10.5.1.0/24"
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -12,18 +12,18 @@ resource "azurerm_network_interface" "" {
 }
 
 resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = "Standard_DS2_v2"
-  admin_username      = ""
+  name                  = ""
+  location              = ""
+  resource_group_name   = ""
+  size                  = "Standard_DS2_v2"
+  admin_username        = ""
   network_interface_ids = []
   admin_ssh_key {
-    username   = ""
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUfrbF0Oco9I8GtJax7rB51qojqrrTz+hT9/4VPZubF84q/A1XOhBPdUEC6C3qeFqrKLiGt/dRndw3dPa+DWFoZB7+rPH/2BVtNG645RSqt/qVbTHS4Tx30VI/cdsrhmGzk/aesJpSuk7tCwjbgsv9n8jp0ZM5sbBNUn6T65p+f2SexYJXyst1wf7G8pvHuhJudkmMy90yLVaI7IDCVKNoSFEC6gQz0vv9M5ayxOOCkYycUXq9D7eW8h69hpdgeU10bTcsF+6MPljb2Krz1Et2KT3o0mtiqKbhZIgbsjJ1l/zOWo2n7Sg9UKdpfFNyd9njzTm1XpvRudkxUWxTU90Lri+jiKx7zBmWLLeC0myN3235KP+POUZS3ChvaeXqTgr9Gl/POeJbfReU6beeSQ21uvHFe4Br/M8C2qzfUDDNWHcBZeYCycniWqldRl04CHIEavhdpPVTccm0mssjorE7VZ83qfJsNwtkFi6axC8+Ys2CA9y3Myd072mjlCh94p8="
   }
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
