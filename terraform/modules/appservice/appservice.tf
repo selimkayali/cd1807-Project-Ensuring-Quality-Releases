8b64b237e13c4cfa12f646ba505cbc6f24f62cdb
selimkayali <selimkayali@gmail.com>
2023-08-06 19:33:14 +0300

    edit

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -19,3 +19,30 @@ resource "azurerm_windows_web_app" "test" {
     always_on = false
   }
 }
+
+resource "azurerm_monitor_action_group" "test" {
+  name                = "app-metric-alert"
+  resource_group_name = var.resource_group
+  short_name          = "app-alert"
+}
+
+resource "azurerm_monitor_metric_alert" "test" {
+  name                 = "app-metric-alert"
+  resource_group_name  = var.resource_group
+  scopes               = [azurerm_windows_web_app.test.id]
+  target_resource_type = "Microsoft.Web/sites"
+  description          = "Metric alert"
+  severity             = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http4xx"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.test.id
+  }
+}
